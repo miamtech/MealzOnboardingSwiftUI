5d4302ba6cf025611feef7f1d1a95b736fc90dbf
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-06-25 10:16:07 +0200

    Initial Commit

---
+++ MealzOnboardingSwiftUI/MealzOnboardingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealzOnboardingSwiftUIApp.swift
+//  MealzOnboardingSwiftUI
+//
+//  Created by Diarmuid McGonagle on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MealzOnboardingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
