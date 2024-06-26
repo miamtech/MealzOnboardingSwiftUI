f7c2ade70e842be9a7daa45e29ecb4dad3254943
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-06-26 10:33:29 +0200

    initialization-solution

---
+++ MealzOnboardingSwiftUI/Mealz/MealzManager.swift
@@ -0,0 +1,14 @@
+//
+//  MealzManager.swift
+//  MealzOnboardingSwiftUI
+//
+//  Created by Diarmuid McGonagle on 26/06/2024.
+//
+
+import Foundation
+
+public class MealzManager {
+  // Will contain calls to Miam SDK core class for modules (User, Basket, Store...)
+  
+  public static let sharedInstance = MealzManager()
+}
